32a7561031a6b784431f3eb1bad7e1efddded29b
Vincent Hardouin <vincenthardouin@icloud.com>
2024-12-08 11:46:21 +0100

    day08 clean

--- day08.ts
+++ day08.ts
@@ -2,7 +2,7 @@ import type { Day } from './Day.ts';
 import { enumGrid } from './utils.ts';
 
 export class DayImpl implements Day {
-  private readonly input: Array<Array<number>>;
+  private readonly input: string[][];
 
   constructor(input: string) {
     this.input = this.parseInput(input);
@@ -19,48 +19,13 @@ export class DayImpl implements Day {
   }
 
   partOne() {
-    const antinodes = new Set();
     const antennas = antennasPositions(this.input);
-    for (const [_, positions] of antennas) {
-      positions.forEach(([y, x], i) => {
-        positions
-          .filter((_, j) => i !== j)
-          .map(([y2, x2]) => {
-            const dy = y2 - y;
-            const dx = x2 - x;
-            return [
-              y + 2 * dy,
-              x + 2 * dx,
-            ];
-          })
-          .filter(([y, x]) => this.input[y]?.[x])
-          .forEach((position: number[]) => antinodes.add(position.join(',')));
-      });
-    }
-    return antinodes.size;
+    return getUniqAntinodes(this.input, antennas, false);
   }
 
   partTwo() {
-    const antinodes = new Set();
     const antennas = antennasPositions(this.input);
-    for (const [_, positions] of antennas) {
-      positions.forEach(([y, x], i) => {
-        positions
-          .filter((_, j) => i !== j)
-          .reduce((acc, [y2, x2]) => {
-            const [dy, dx] = [y2 - y, x2 - x];
-            let [py, px] = [y + dy, x + dx];
-            while (this.input[py]?.[px]) {
-              acc.push([py, px]);
-              [py, px] = [py + dy, px + dx];
-            }
-            return acc;
-          }, [])
-          .filter(([y, x]) => this.input[y]?.[x])
-          .forEach((position: number[]) => antinodes.add(position.join(',')));
-      });
-    }
-    return antinodes.size;
+    return getUniqAntinodes(this.input, antennas, true);
   }
 }
 
@@ -76,4 +41,34 @@ function antennasPositions(grid: string[][]) {
     antennas.get(cell).push([x, y]);
   }
   return antennas;
+}
+
+function getUniqAntinodes(grid: string[][], antennas: Map<string, number[][]>, extended: boolean): number {
+  const antinodes = new Set();
+  for (const [_, positions] of antennas) {
+    positions.forEach(([y, x], i) => {
+      positions
+        .filter((_, j) => i !== j)
+        .reduce((acc, [y2, x2]) => {
+          const [dy, dx] = [y2 - y, x2 - x];
+
+          const k = extended ? 1 : 2;
+          let [py, px] = [y + k * dy, x + k * dx];
+
+          if (!extended) {
+            acc.push([py, px]);
+            return acc;
+          }
+
+          while (grid[py]?.[px]) {
+            acc.push([py, px]);
+            [py, px] = [py + dy, px + dx];
+          }
+          return acc;
+        }, [] as number[][])
+        .filter(([y, x]) => grid[y]?.[x])
+        .forEach((position: number[]) => antinodes.add(position.join(',')));
+    });
+  }
+  return antinodes.size;
 }
